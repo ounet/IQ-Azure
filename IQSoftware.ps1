b825bfc706b2b306d6c4bb4304d780e274ceaf3c
ounet <ounet@outlook.com>
2024-06-14 13:40:56 -0400

    update

--- IQSoftware.ps1
+++ IQSoftware.ps1
@@ -1,36 +1,54 @@
+<#Author       : Jean-Marc Pigeon
+# Usage        : Install software for IQ AVD
+#>
 
-function Write-Log {
-    param(
-        [string]$Message
-    )
-     $timestamp = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
-     "$timestamp - $Message" | Out-File -FilePath \\iqpocstorage.file.core.windows.net\avdcustomimagetemplateslogs\avdcit.logs -Append
+
+####################################
+#    Test/Create Temp Directory    #
+####################################
+if((Test-Path c:\IQ) -eq $false) {
+    Write-Host "AVD AIB Customization - Creating temp directory"
+    New-Item -Path c:\IQ -ItemType Directory
+}
+else {
+    Write-Host "AVD AIB Customization - c:\IQ already exists"
 }
 
-<#logs files#>
+######################
+#    WVD Variables   #
+######################
+$LocalWVDpath            = "c:\IQ\"
+$FSInstaller             = 'npp.8.6.8.Installer.x64.exe'
+$templateFilePathFolder = "C:\AVDImage"
 
-Write-Log "mounting z drive"
-
-New-Item -Path "c:\" -Name "IQ" -ItemType "directory"
-Write-Log "create iq directories"
 
 #Download notepad++
 $Filename = "npp.8.6.8.Installer.x64.exe"
-Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.6.8/npp.8.6.8.Installer.x64.exe" -OutFile $Filename
-Write-Log "Download $Filename"
+Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.6.8/npp.8.6.8.Installer.x64.exe" -OutFile "$localwvdpath$Filename"
+
 #Install notepad++
-Start-Process $Filename -ArgumentList "/S" -Wait
-Write-Log "installation de $Filename"
+#########################
+Write-Host "AVD AIB Customization - Install FSLogix : Starting to install notepad++"
+$fslogix_deploy_status = Start-Process `
+    -FilePath "$LocalWVDpath\$Filename" `
+    -ArgumentList '/S' `
+    -Wait `
+    -Passthru
+
 
 #Remove install file
-Remove-Item $Filename
+#Remove-Item $Filename
 
 #Download powershell
-$Filename = "powershell7.msi"
-Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.4.1/PowerShell-7.4.1-win-x64.msi" -OutFile $Filename
+$Filename1 = "powershell7.msi"
+Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.4.1/PowerShell-7.4.1-win-x64.msi" -OutFile "$LocalWVDpath\$Filename"
 
 #Install powershell silently
-Start-Process msiexec.exe -Wait -ArgumentList "/package $($Filename) /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1"
+$fslogix_deploy_status = Start-Process `
+    -FilePath "msiexec.exe" `
+    -ArgumentList "/package $($Filename1) /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1" `
+    -Wait
+ 
 
 #Remove install file
-Remove-Item $Filename
+#Remove-Item $Filename1
